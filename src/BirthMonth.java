37a58167be1ae36612f67efaee0eea5ba4c4f1eb
Ibrokhimm0752 <Ibrokhimm0752@hstat.org>
2025-02-14 18:25:56 -0500

    updated

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
 public class BirthMonth {
     public static void main(String[] args) {
-        int birthMonth = 20;
-        if (birthMonth >= 1 && birthMonth <= 12) {
-            System.out.println("Your birth month is: " + birthMonth);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your birth month (1-12): ");
+        if (scanner.hasNextInt()) {
+            int birthMonth = scanner.nextInt();
+            if (birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("Your birth month is: " + birthMonth);
+            } else {
+                System.out.println("You entered an incorrect month value: " + birthMonth);
+            }
         } else {
-            System.out.println("You entered an incorrect month value: " + birthMonth);
+            System.out.println("Invalid input. Please enter an integer.");
         }
+        scanner.close();
     }
 }
